5431e8c92e34d43994b225bdefeca7e733626021
Lee Lacanlale <lee.lacanlale@gmail.com>
2019-10-17 14:06:02 -0700

    file upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+
+const random = document.querySelector(".buttonRand");
+
+function setGradient() {
+    let body = document.getElementById("gradient");
+    let css = document.querySelector("h3");
+
+    body.style.background = "linear-gradient(to right, " 
+        + color1.value + ", " + color2.value + ")";
+    
+        css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+
+    for (var i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", function() {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+
+    setGradient();
+});
